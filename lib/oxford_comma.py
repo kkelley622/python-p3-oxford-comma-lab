5f5cf1636adb2981e8bf553c6b155504131d8997
Kevin Kelley <kkelley622@gmail.com>
2023-12-19 15:18:21 -0600

    All tests are passing.

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
-    return None
+    if not items:
+        return ''
+    
+    string_end = items.pop()
+    if not items:
+        return string_end
+    
+    and_display = ' and ' if len(items) == 1 else ', and '
+    new_string = ', '.join(items) + and_display + string_end
+
+    return new_string
+
